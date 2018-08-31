077ee6b8a2557f624ef82890358d5ed6a8f3fa35
sf0x <fox.troiza@gmail.com>
2018-08-31 04:33:56 +0200

    c template added
    self written template for pbkdf2

---
+++ hmac.h
@@ -0,0 +1,10 @@
+#ifndef HMAC_H_
+#define HMAC_H_
+#include <stdint.h>
+#include <stdio.h>
+#include "sha256.h"
+
+void hmac(uint32_t key[8], uint32_t input[8], uint32_t hash[8], unsigned int round);
+
+
+#endif
